97805ee71fa542dd1ecf1a19bf3fd118a2135077
yw05 <y5nw@outlook.com>
2020-04-18 20:39:46 +0200

    Add support for headings

--- markdown.php
+++ markdown.php
@@ -5,6 +5,10 @@ function parseMD($string){
 	$ret = $string;
 	/* <code> */
 	$ret = preg_replace('/`([^`]+)`/ms', '<code>${1}</code>', $ret);
+	/* <h1> .. <h6> */
+	for ($i = 1; $i < 7; $i++){
+		$ret = preg_replace('/^'.str_repeat('#', $i).' +(.*)$', '<h'.$i.'>${1}</h'.$i.'>');
+	}
 	return $ret;
 }
 
