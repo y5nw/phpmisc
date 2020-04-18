7a50f42ddb142365552685a1b8b71f531f791f05
yw05 <y5nw@outlook.com>
2020-04-18 20:52:03 +0200

    Parse syntax for images and links in Markdown

--- markdown.php
+++ markdown.php
@@ -9,6 +9,9 @@ function parseMD($string){
 	for ($i = 1; $i < 7; $i++){
 		$ret = preg_replace('/^'.str_repeat('#', $i).' +(.*)$/', '<h'.$i.'>${1}</h'.$i.'>', $ret);
 	}
+	/* <img>  and <a> */
+	$ret = preg_replace('/!\[.*\]\(.*\)/', '<img src="${2}" alt="${1}"></img>', $ret);
+	$ret = preg_replace('/\[.*\]\(.*\)/', '<a href="${2}">$1</a>', $ret);
 	return $ret;
 }
 
