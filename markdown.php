7b6e3d4487942a0d287a2b183f58b05721848c30
yw05 <y5nw@outlook.com>
2020-04-18 21:18:20 +0200

    Allow MD parser configuration

--- markdown.php
+++ markdown.php
@@ -1,17 +1,25 @@
 <?php
 
+include 'config.php';
+
 // Since this function is called by mkXML*, it is a bad idea to use them.
 function parseMD($string){
+	$mdconfig = PHPMISC_CONFIG['content']['markdown'];
+	if (!$mdconfig['enabled']) return $string;
 	$ret = $string;
 	/* <code> */
-	$ret = preg_replace('/`([^`]+)`/ms', '<code>${1}</code>', $ret);
+	if ($mdconfig['code']) $ret = preg_replace('/`([^`]+)`/ms', '<code>${1}</code>', $ret);
 	/* <h1> .. <h6> */
-	for ($i = 1; $i < 7; $i++){
+	if ($mdconfig['headings']) for ($i = 1; $i < 7; $i++){
 		$ret = preg_replace('/^'.str_repeat('#', $i).' +(.*)$/', '<h'.$i.'>${1}</h'.$i.'>', $ret);
 	}
 	/* <img>  and <a> */
-	$ret = preg_replace('/!\[([^\]]*)\]\(([^\)]*)\)/', '<img src="${2}" alt="${1}"></img>', $ret);
-	$ret = preg_replace('/\[([^\]]*)\]\(([^\)]*)\)/', '<a href="${2}">$1</a>', $ret);
+	if ($mdconfig['images']){
+		$ret = preg_replace('/!\[([^\]]*)\]\(([^\)]*)\)/', '<img src="${2}" alt="${1}"></img>', $ret);
+		$ret = preg_replace('/\[([^\]]*)\]\(([^\)]*)\)/', '<a href="${2}">$1</a>', $ret);
+	}
+	/* Extra: octicons */
+	if ($mdconfig['octicons']) $ret = preg_replace('/::o::([^:]+)::/', octicons('${1}'), $ret);
 	return $ret;
 }
 
