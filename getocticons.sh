f3f011867eb9b78f88f6b9ffa1af1c2343ff0d55
yw05 <y5nw@outlook.com>
2020-04-18 22:43:57 +0200

    Remove conversion to object

--- getocticons.sh
+++ getocticons.sh
@@ -6,6 +6,6 @@ if [ $AGREE_LICENSE == false ]; then
 	echo "Please make sure that you agree to the license of Octicons before preceeding.";
 	echo "To make sure that you read this message, edit this script and change \"false\" to \"true\"";
 else
-	OCTICONS_DATA=$(echo "<?php var_export(json_decode('$(wget -O- https://unpkg.com/octicons/build/data.json)')); ?>" | php)
+	OCTICONS_DATA=$(echo "<?php var_export(json_decode('$(wget -O- https://unpkg.com/octicons/build/data.json)')); ?>" | php | sed -E 's/(object)//g')
 	echo "<?php \$PHPMISC_OCTICONS_DATA=${OCTICONS_DATA}; ?>" > octicons.php
 fi
